d90c436fba50e1a8a1a8218673e97c1db0a000d2
Sobolev Mikhail <sobolev_mv@nlmk.com>
2021-11-23 22:42:06 +0500

    Доработан отчет "Продольная обрезь УО"

--- Viz.WrkModule.MagLab/ViewModel/ViewModelMagLab.cs
+++ Viz.WrkModule.MagLab/ViewModel/ViewModelMagLab.cs
@@ -2,8 +2,6 @@
 using System.Data;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Windows.Controls;
 using DevExpress.XtraEditors.DXErrorProvider;
 using Smv.MVVM.Commands;
