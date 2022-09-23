f982e41946e4d049bbed99d324c2067c551cd2fb
Sobolev Mikhail <sobolev_mv@nlmk.com>
2022-09-23 15:25:06 +0500

    Разработка отчета "Ощй УСТ"
    Реализована часть отчета  - Данные по ЦХП.

--- Viz.WrkModule.Qc.Db/Reports.cs
+++ Viz.WrkModule.Qc.Db/Reports.cs
@@ -14,6 +14,7 @@ using Smv.Data.Oracle;
 using Smv.Utils;
 using Viz.WrkModule.Qc.Db.Dto;
 using System.Security.Cryptography;
+using DevExpress.XtraSpreadsheet.Model;
 
 namespace Viz.WrkModule.Qc.Db
 {
@@ -71,24 +72,61 @@ namespace Viz.WrkModule.Qc.Db
       var workBook = new Workbook();
       workBook.LoadDocument(src, DocumentFormat.Xltx);
       var workSheet = workBook.Worksheets.ActiveWorksheet = workBook.Worksheets[0];
-      
-      const string sqlStmt1 = "select NAMEGROUP, RATIO_GROUP from VIZ_PRN.V_QMF_RPTGRNUST_WORKSHOP";
+
+      workSheet[4, 1].Value = dtoRpt.DateFrom;
+      workSheet[4, 2].Value = dtoRpt.DateTo;
+      workSheet[5, 1].Value = dtoRpt.FinalThicknessSql;
+      workSheet[6, 1].Value = dtoRpt.IsKesiAvg ? dtoRpt.KesiAvgMin : (int?)null;
+      workSheet[6, 2].Value = dtoRpt.IsKesiAvg ? dtoRpt.KesiAvgMax : (int?)null;
+      workSheet[7, 1].Value = dtoRpt.IsKesiWorst ? dtoRpt.KesiWorstMin : (int?)null;
+      workSheet[7, 2].Value = dtoRpt.IsKesiWorst ? dtoRpt.KesiWorstMax : (int?)null;
+      workSheet[8, 1].Value = dtoRpt.IsP1750 ? dtoRpt.P1750Min : (double?)null;
+      workSheet[8, 2].Value = dtoRpt.IsP1750 ? dtoRpt.P1750Max : (double?)null;
+      workSheet[9, 1].Value = dtoRpt.IsDefectTolowCat ? dtoRpt.DefectTolowCat : string.Empty;
+      workSheet[10, 1].Value = dtoRpt.IsDefectTo2Sort ? dtoRpt.DefectTo2Sort : string.Empty;
+      workSheet[11, 1].Value = dtoRpt.IsAdgIn ? dtoRpt.AdgInSql : string.Empty;
+
+      const string sqlStmt1 = "select NAMEGROUP, RATIO_GROUP from VIZ_PRN.V_QMF_RPTGRNDFF_WS";
       var odr = Odac.GetOracleReader(sqlStmt1, CommandType.Text, false, null, null);
       if (odr != null)
       {
         int flds = odr.FieldCount;
-        int row = 4;
+        int row = 16;
 
         while (odr.Read())
         {
           workSheet[row, 0].Value = odr.GetString(0);
-          workSheet[row, 1].Value = odr.GetDouble(1);
-
+          workSheet[row, 2].Value = odr.GetDouble(1);
           row++;
         }
         odr.Close();
         odr.Dispose();
       }
+
+      const string sqlStmt2 = "select NAMEGROUP, RATIO_GROUP from VIZ_PRN.V_QMF_RPTGRNUST_WS";
+      odr = Odac.GetOracleReader(sqlStmt2, CommandType.Text, false, null, null);
+      if (odr != null)
+      {
+        int flds = odr.FieldCount;
+        int row = 16;
+
+        while (odr.Read())
+        {
+          workSheet[row, 0].Value = odr.GetString(0);
+          workSheet[row, 1].Value = odr.GetDouble(1);
+          row++;
+        }
+        odr.Close();
+        odr.Dispose();
+      }
+
+      var valUstAll = Convert.ToDouble(Odac.ExecuteScalar("select RATIO_ALL from VIZ_PRN.V_QMF_RPTGRNUST_WS", CommandType.Text, false, null));
+      var valDffAll = Convert.ToDouble(Odac.ExecuteScalar("select RATIO_ALL from VIZ_PRN.V_QMF_RPTGRNDFF_WS", CommandType.Text, false, null));
+      var charTitle = $"ЦХП, УСТ -  {valUstAll:n2}; КНД - {valDffAll:n2}";
+      workSheet.Charts[0].Title.SetValue(charTitle);
+
+
+
 
       workBook.SaveDocument(dst, DocumentFormat.Xlsx);
       workBook.Dispose();
