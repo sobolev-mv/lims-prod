0bd17cfe2e2eb32c2e366c1eb865bec00626b1a6
Sobolev Mikhail <sobolev_mv@nlmk.com>
2022-09-24 09:08:52 +0500

    Revert "Перевод проектва на DX22.1.3.0 и dotConnect 10.0.0.0"
    This reverts commit 54a3c5bc4c66de2d829c7651a37871c4d6ec9517.

--- Viz.WrkModule.Qc.Db/Dto/DtoRptGnrUstParamInput.cs
+++ Viz.WrkModule.Qc.Db/Dto/DtoRptGnrUstParamInput.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Effects;
 
 namespace Viz.WrkModule.Qc.Db.Dto
 {
